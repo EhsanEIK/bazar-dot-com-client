c90e93c23c66b86622475e479bf321afdae6f316
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-12-07 10:40:08 +0600

    updated About page

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h1>This is About</h1>
-        </div>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src="https://media.istockphoto.com/id/1086341762/vector/about-us-flat-design-style-colorful-illustration.jpg?s=612x612&w=0&k=20&c=jeUDlovR0TRmuSb6lp19P7ijHx8UNtZzZhHJZVmoWJc=" className="rounded-lg shadow-2xl" alt='' />
+                <div>
+                    <h1 className="text-5xl font-bold">About Us</h1>
+                    <p className="py-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex autem aut culpa odio repudiandae dolorem laudantium fuga doloribus commodi magni modi quis necessitatibus ad hic nihil tempore quae optio blanditiis placeat, sit eos incidunt? Error porro reiciendis, perspiciatis necessitatibus tempora doloremque doloribus dolorum, magni quae delectus maiores minima modi neque!</p>
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+            </div >
+        </div >
     );
 };
 
